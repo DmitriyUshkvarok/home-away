2973f9f28a959eaaef4f97a96c33a0277b6d3b1b
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2025-01-04 17:34:42 +0200

    добавлены компоненты для отображения статистики и графиков бронирований; реализована логика получения данных для статистики и графиков; добавлены загрузочные состояния для компонентов

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -12,6 +12,7 @@ import {
 } from '@/utils/schemas';
 import { uploadImage } from '@/utils/supabase';
 import { calculateTotals } from '@/utils/calculateTotals';
+import { formatDate } from '@/utils/format';
 
 const renderError = (error: unknown): { message: string } => {
   console.log(error);
@@ -28,6 +29,12 @@ const getAuthUser = async () => {
   }
 
   //   if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+  return user;
+};
+
+const getAdminUser = async () => {
+  const user = await getAuthUser();
+  if (user.id !== process.env.ADMIN_USER_ID) redirect('/');
   return user;
 };
 
@@ -628,4 +635,51 @@ export const fetchReservations = async () => {
     },
   });
   return reservations;
+};
+
+export const fetchStats = async () => {
+  await getAdminUser();
+
+  const usersCount = await db.profile.count();
+  const propertiesCount = await db.property.count();
+  const bookingsCount = await db.booking.count();
+
+  return {
+    usersCount,
+    propertiesCount,
+    bookingsCount,
+  };
+};
+
+export const fetchChartsData = async () => {
+  await getAdminUser();
+  const date = new Date();
+  date.setMonth(date.getMonth() - 6);
+  const sixMonthsAgo = date;
+
+  const bookings = await db.booking.findMany({
+    where: {
+      createdAt: {
+        gte: sixMonthsAgo,
+      },
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+  });
+  const bookingsPerMonth = bookings.reduce(
+    (total, current) => {
+      const date = formatDate(current.createdAt, true);
+
+      const existingEntry = total.find((entry) => entry.date === date);
+      if (existingEntry) {
+        existingEntry.count += 1;
+      } else {
+        total.push({ date, count: 1 });
+      }
+      return total;
+    },
+    [] as Array<{ date: string; count: number }>
+  );
+  return bookingsPerMonth;
 };
--- src/components/admin/ChartsContainer.tsx
+++ src/components/admin/ChartsContainer.tsx
@@ -1,5 +1,10 @@
-const ChartsContainer = () => {
-  return <div></div>;
+import { fetchChartsData } from '@/actions/actions';
+import Chart from './Chart';
+
+const ChartsContainer = async () => {
+  const bookings = await fetchChartsData();
+  if (bookings.length < 1) return null;
+  return <Chart data={bookings} />;
 };
 
 export default ChartsContainer;
