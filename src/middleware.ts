8d8c9f468739c53629276288558ad28d63542f6a
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2025-01-07 14:11:22 +0200

    восстановлены проверки маршрутов для администраторов в middleware, улучшена логика обработки доступа

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(['/', '/properties(.*)']);
-// const isAdminRoute = createRouteMatcher(['/admin(.*)']);
+const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
-  // const isAdminUser = (await auth()).userId === process.env.ADMIN_USER_ID;
-  // if (isAdminRoute(req) && !isAdminUser) {
-  //   return NextResponse.redirect(new URL('/', req.url));
-  // }
+  const isAdminUser = (await auth()).userId === process.env.ADMIN_USER_ID;
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
   if (!isPublicRoute(req)) await auth.protect();
 });
 
