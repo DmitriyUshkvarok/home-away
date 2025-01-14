9b5433515eb297535dfa68e56b6fc5dc284b7362
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2025-01-14 12:53:08 +0200

    обновлены пути в файлах robots.txt и sitemap.ts для корректной генерации карты сайта

--- src/app/[locale]/sitemap.ts
+++ src/app/[locale]/sitemap.ts
@@ -1,7 +1,7 @@
 import db from '@/utils/db';
 
 export default async function sitemap() {
-  const baseUrl = 'https://home-away-snowy.vercel.app/en';
+  const baseUrl = 'https://home-away-snowy.vercel.app';
   const properties = await db.property.findMany({});
 
   const propertiesUrl = properties?.map((property) => {
